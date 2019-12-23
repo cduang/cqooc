9fedd36be3979d7e0b3340dbe84dffe925bdacd4
Rekols <rekols@foxmail.com>
2019-12-24 01:10:54 +0800

    Update cqooc.py

--- cqooc.py
+++ cqooc.py
@@ -14,44 +14,85 @@ postapi='http://www.cqooc.com/learnLog/api/add'
 session = requests.Session()
 session.cookies['xsid'] = cookie_xsid
 
-def fuck(courseId, sectionId, chapterId):
-    cookie = {
-            'player':'1',
-            'xsid': cookie_xsid
+def fuck_time():
+    head={
+        'Host': 'www.cqooc.com',
+        'Content-Length': '30',
+        'Connection': 'keep-alive',
+        'Origin': 'http://www.cqooc.com',
+        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+        'Content-Type': 'application/json',
+        'Accept': '*/*',
+        'Referer': 'http://www.cqooc.com/learn/mooc/structure?id=' + courseId,
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cookie': 'player=1; xsid=' + cookie_xsid,
     }
+    data = {
+        "username": username
+    }
+    ps = requests.post(url='http://www.cqooc.com/account/session/api/login/time', json=data, headers=head, timeout=None)
+    print(ps.text)
+
+def get_log(sectionId):
+    head={
+        'Host': 'www.cqooc.com',
+        'Connection': 'keep-alive',
+        'Origin': 'http://www.cqooc.com',
+        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
+        'Accept': '*/*',
+        'Referer': 'http://www.cqooc.com/learn/mooc/structure?id=' + courseId,
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cookie': 'player=1; xsid=' + cookie_xsid,
+    }
+    r = requests.get('http://www.cqooc.com/json/learnLogs?sectionId=' + sectionId + '&username=' + username, headers=head)
+    print(r.text)
+
+def fuck(courseId, sectionId, chapterId):
     head={
             'Host': 'www.cqooc.com',
-            'Content-Length': '159',
+            'Content-Length': '156',
+            'Connection': 'keep-alive',
             'Origin': 'http://www.cqooc.com',
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
+            'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36",
             'Content-Type': 'application/json',
             'Accept': '*/*',
             'Referer': 'http://www.cqooc.com/learn/mooc/structure?id=' + courseId,
             'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6',
-            'Cookie': 'xsid=' + cookie_xsid + '; player=1',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cookie': 'player=1; xsid=' + cookie_xsid,
     }
 
     data={
-        "username": username, "ownerId": ownerId, "parentId": parentId, "action": 0,
-        "courseId": courseId, "sectionId": sectionId, "chapterId": chapterId, "category": "2",        
+        "action": 0, "category": 2, "chapterId": str(chapterId), "courseId": str(courseId), "ownerId": ownerId,
+        "parentId": str(parentId), "sectionId": int(sectionId), "username": username
     }
+
+    fuck_time()
+    get_log(sectionId)
+    time.sleep(2)
+    get_log(sectionId)
+    time.sleep(2)
+    get_log(sectionId)
+    time.sleep(2)
+    fuck_time()
+    time.sleep(1)
     ps = requests.post(url=postapi, json=data, headers=head, timeout=None)
-    #time.sleep(5)
+    print('starting', chapterId, sectionId)
     print('current sectionId: ' + sectionId)
     print('current chapterId: ' + chapterId)
     print(ps.reason)
     print(ps.cookies)
     print(ps.text)
     print('\n')
-    #time.sleep(10)
+    
 
 def learn_course(courseId):
     endpoint = 'http://www.cqooc.com/json/chapter/lessons?courseId='+courseId
     j = session.get(endpoint).json()['data'][0]['body']
     for chapterId, sectionIds in j.items():
         for sectionId in sectionIds:
-            print('starting', chapterId, sectionId)
             fuck(courseId, sectionId, chapterId)
             # print(fuck(courseId, sectionId, chapterId).text)
 
